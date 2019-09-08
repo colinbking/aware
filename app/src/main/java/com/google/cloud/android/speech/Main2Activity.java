8ec3ca8ac61ea9896ff85589f4a89b75e16d6428
Timothy Goh <tmg5@rice.edu>
2019-09-08 00:02:31 -0400

    Added new landing page

--- app/src/main/java/com/google/cloud/android/speech/Main2Activity.java
+++ app/src/main/java/com/google/cloud/android/speech/Main2Activity.java
@@ -1,13 +1,35 @@
 package com.google.cloud.android.speech;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final Button button = (Button) findViewById(R.id.beginButton);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.chime);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+                Intent activity2Intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(activity2Intent);
+            }
+        });
+//        button.performClick();
     }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
